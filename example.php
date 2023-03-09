9139e9d164850e22d8c9d62e39378f0adf396a2d
Pinga <121483313+getpinga@users.noreply.github.com>
2023-03-09 17:33:02 +0200

    Update example.php

--- example.php
+++ example.php
@@ -256,7 +256,6 @@ if ($a == 'cart') {
 		<?php endif; ?>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-
 		<script>
 			$(document).ready(function(){
 				$('.add-to-cart').on('click', function(e){
